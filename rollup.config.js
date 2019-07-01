d6b7c1322457d6fb617b33364f63bae0adaf033b
Maxim Matyunin <matyunya@gmail.com>
2019-07-02 01:00:55 +0900

    rename

--- rollup.config.js
+++ rollup.config.js
@@ -14,28 +14,28 @@ const name = pkg.name
   .replace(/-\w/g, (m) => m[1].toUpperCase());
 
 export default {
-  input: !production ? 'src/main.js' : 'src/App.svelte',
+  input: !production ? 'src/main.js' : 'src/Waypoint.svelte',
   output: !production
     ? {
-        sourcemap: true,
-        format: 'iife',
-        name: 'app',
-        file: 'public/bundle.js',
-      }
+      sourcemap: true,
+      format: 'iife',
+      name: 'app',
+      file: 'public/bundle.js',
+    }
     : [
-        {
-          file: pkg.module,
-          format: 'es',
-          sourcemap: true,
-          name,
-        },
-        {
-          file: pkg.main,
-          format: 'umd',
-          sourcemap: true,
-          name,
-        },
-      ],
+      {
+        file: pkg.module,
+        format: 'es',
+        sourcemap: true,
+        name,
+      },
+      {
+        file: pkg.main,
+        format: 'umd',
+        sourcemap: true,
+        name,
+      },
+    ],
   plugins: [
     babel({
       runtimeHelpers: true,
